7235fc1012619041421ef90be1951f8c3f65e6dd
Lorenzo Di Gaetano <lorenzo.digaetano@istat.it>
2021-01-29 00:17:43 +0100

    Inizio suono e ritocco sfondo

--- rana.s
+++ rana.s
@@ -181,6 +181,25 @@ InitLevel:
 
 ;    bsr.w   DrawScore
 
+; INIZIO PROVA AUDIO
+
+    
+    lea     Cra,a0
+    move.l  a0,$dff0d0      ; Sample in AUD3LC
+    move.w  #3000,$dff0d4  ; Lunghezza in word in AUD3LEN
+    move.w  #300,$dff0d6    ; Periodo in AUD3PER
+    move.w  #64,$dff0d8     ; Volume massimo in AUD3VOL
+    move.w  #%1000000000001000,$dff096      ; DMA audio 2 attivo in DMACON
+
+    bsr.w   wframe
+
+    lea     Silent,a0
+    move.l  a0,$dff0d0
+    move.w  #100,$dff0d4  ; Lunghezza in word in AUD2LEN
+; FINE PROVA AUDIO
+
+
+
 ; GAME LOOP
 
     move.l  #0,d0
@@ -480,9 +499,13 @@ RanaX:
 RanaY:
     dc.w    50
 
+; AUDIO
 
-
-
+Cra:
+;    incbin  "sfx/cra.raw"
+    incbin  "sfx/cra_11025.raw"
+Silent:
+    dcb.w   100
 Lifes:
     dc.w    3
 
