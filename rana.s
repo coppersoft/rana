7ed9c28ba9e6eee77e56fe8af4de87e6de27471c
Lorenzo Di Gaetano <lorenzo.digaetano@istat.it>
2021-02-09 20:47:29 +0100

    Palette con giallo nell'esplosione

--- rana.s
+++ rana.s
@@ -204,7 +204,7 @@ InitLevel:
 
 	move.w	#50,d0
 	move.w	#50,d1
-	move.w	#12,d5
+	move.w	#5,d5
 	bsr.w	ShowExplosionFrame
 
 
@@ -703,7 +703,7 @@ Bplpointers:
 PaletteRaw:
 	dc.w	$0000,$0ccc,$0999,$0666,$0222,$00e0,$00b0,$0070,$00d3,$00dd
 	dc.w	$008c,$003b,$0009,$070f,$0c0e,$0c08,$0620,$0e52,$0a52,$0005
-	dc.w	$0038,$007b,$00dd,$0333,$0888,$0ddd,$0e52,$0a30,$0620,$0080
+	dc.w	$0038,$007b,$00dd,$0333,$0888,$0ff0,$0e52,$0a30,$0620,$0080
 	dc.w	$03c0,$08f0
 
 FadeInFrame:
