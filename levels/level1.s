ce356df42b7525b19d839c431d716ebefcb52ec3
Lorenzo Di Gaetano <lorenzodigaetano@yahoo.it>
2021-04-02 01:20:13 +0200

    Aggiunta cartella con i livelli

---
+++ levels/level1.s
@@ -0,0 +1,322 @@
+; Struttura dati per i livelli
+
+; - Indirizzo bob
+; - Indirizzo bobmask
+; - larghezza in word
+; - velocità (con segno)
+; - x attuale
+; - x (iniziale)
+; - y
+; - contatore fotogramma
+; - fotogramma attuale
+; - fotogrammi totali
+
+	dc.l	Tronco1			; Indirizzo bob
+	dc.l	Tronco1_mask	; Indirizzo bobmask
+	dc.w	(64/16)			; Larghezza in word
+	dc.w	1				; Velocità
+	dc.w	0				; x
+	dc.w	50				; y
+	dc.l	SingleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+
+	dc.l	Tronco2			; Indirizzo bob
+	dc.l	Tronco2_mask	; Indirizzo bobmask
+	dc.w	(96/16)			; Larghezza in word
+	dc.w	1				; Velocità
+	dc.w	64				; x
+	dc.w	50				; y
+	dc.l	SingleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+	dc.l	Tronco1			; Indirizzo bob
+	dc.l	Tronco1_mask	; Indirizzo bobmask
+	dc.w	(64/16)			; Larghezza in word
+	dc.w	1				; Velocità
+	dc.w	64+96+96		; x
+	dc.w	50				; y
+	dc.l	SingleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+; Riga 2
+
+	dc.l	Turtle			; Indirizzo bob
+	dc.l	Turtle_mask		; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	10		; x
+	dc.w	70				; y
+	dc.l	NormalTurtleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+	dc.l	Turtle			; Indirizzo bob
+	dc.l	Turtle_mask		; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	10+36	; x
+	dc.w	70				; y
+	dc.l	NormalTurtleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+	dc.l	Turtle			; Indirizzo bob
+	dc.l	Turtle_mask		; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	10+36+36	; x
+	dc.w	70				; y
+	dc.l	NormalTurtleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+
+
+
+	dc.l	Turtle			; Indirizzo bob
+	dc.l	Turtle_mask		; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	10+64+96+96		; x
+	dc.w	70				; y
+	dc.l	TurtleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+	dc.l	Turtle			; Indirizzo bob
+	dc.l	Turtle_mask		; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word;
+	dc.w	-1				; Velocità
+	dc.w	10+64+96+96+36	; x
+	dc.w	70				; y
+	dc.l	TurtleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+	dc.l	Turtle			; Indirizzo bob
+	dc.l	Turtle_mask		; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	10+64+96+96+36+36	; x
+	dc.w	70				; y
+	dc.l	TurtleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+; Riga 3
+
+	dc.l	Tronco1			; Indirizzo bob
+	dc.l	Tronco1_mask	; Indirizzo bobmask
+	dc.w	(64/16)			; Larghezza in word
+	dc.w	1				; Velocità
+	dc.w	30				; x
+	dc.w	90				; y
+	dc.l	SingleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+
+	dc.l	Tronco2			; Indirizzo bob
+	dc.l	Tronco2_mask	; Indirizzo bobmask
+	dc.w	(96/16)			; Larghezza in word
+	dc.w	1				; Velocità
+	dc.w	30+64+96		; x
+	dc.w	90				; y
+	dc.l	SingleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+	dc.l	Tronco1			; Indirizzo bob
+	dc.l	Tronco1_mask	; Indirizzo bobmask
+	dc.w	(64/16)			; Larghezza in word
+	dc.w	1				; Velocità
+	dc.w	30+64+96+96		; x
+	dc.w	90				; y
+	dc.l	SingleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+; Riga 4
+
+
+; tartarughe
+
+	dc.l	Turtle			; Indirizzo bob
+	dc.l	Turtle_mask		; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	30		; x
+	dc.w	110				; y
+	dc.l	TurtleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+	dc.l	Turtle			; Indirizzo bob
+	dc.l	Turtle_mask		; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	30+36	; x
+	dc.w	110				; y
+	dc.l	TurtleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+	dc.l	Turtle			; Indirizzo bob
+	dc.l	Turtle_mask		; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	30+36+36	; x
+	dc.w	110				; y
+	dc.l	TurtleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+
+
+
+	dc.l	Turtle			; Indirizzo bob
+	dc.l	Turtle_mask		; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	30+64+96+96		; x
+	dc.w	110				; y
+	dc.l	NormalTurtleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+	dc.l	Turtle			; Indirizzo bob
+	dc.l	Turtle_mask		; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word;
+	dc.w	-1				; Velocità
+	dc.w	30+64+96+96+36	; x
+	dc.w	110				; y
+	dc.l	NormalTurtleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+	dc.l	Turtle			; Indirizzo bob
+	dc.l	Turtle_mask		; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	30+64+96+96+36+36	; x
+	dc.w	110				; y
+	dc.l	NormalTurtleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+
+
+
+
+; Riga 1 automobili
+
+	dc.l	FurgoneSx			; Indirizzo bob
+	dc.l	FurgoneSx_mask	; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	320				; x
+	dc.w	160				; y
+	dc.l	SingleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+	dc.l	AutoBluSx			; Indirizzo bob
+	dc.l	AutoBluSx_mask	; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	160				; x
+	dc.w	160				; y
+	dc.l	SingleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+
+	dc.l	FurgoneSx			; Indirizzo bob
+	dc.l	FurgoneSx_mask	; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	100				; x
+	dc.w	160				; y
+	dc.l	SingleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+	dc.l	AutoArancioneSx			; Indirizzo bob
+	dc.l	AutoArancioneSx_mask	; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	50				; x
+	dc.w	160				; y
+	dc.l	SingleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+; Riga 2 autobili
+
+;	dc.l	Buggy			; Indirizzo bob
+;	dc.l	Buggy_mask	; Indirizzo bobmask
+;	dc.w	(48/16)			; Larghezza in word
+;	dc.w	-4				; Velocità
+;	dc.w	50				; x
+;	dc.w	190				; y
+;	dc.l	SingleFrameList	; Lista fotogrammi
+;	dc.w	0				; Fotogramma attuale
+
+	dc.l	RoverDx
+	dc.l	RoverDx_mask
+	dc.w	(48/16)
+	dc.w	1
+	dc.w	50
+	dc.w	190
+	dc.l	SingleFrameList
+	dc.w	0
+
+	dc.l	AutoBluDx
+	dc.l	AutoBluDx_mask
+	dc.w	(48/16)
+	dc.w	1
+	dc.w	100
+	dc.w	190
+	dc.l	SingleFrameList
+	dc.w	0
+
+	dc.l	AutoArancioneDx
+	dc.l	AutoArancioneDx_mask
+	dc.w	(48/16)
+	dc.w	1
+	dc.w	150
+	dc.w	190
+	dc.l	SingleFrameList
+	dc.w	0
+
+	dc.l	RoverDx
+	dc.l	RoverDx_mask
+	dc.w	(48/16)
+	dc.w	1
+	dc.w	200
+	dc.w	190
+	dc.l	SingleFrameList
+	dc.w	0
+
+; Riga 3 autobili
+
+	dc.l	FurgoneSx			; Indirizzo bob
+	dc.l	FurgoneSx_mask	; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	40				; x
+	dc.w	220				; y
+	dc.l	SingleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+	dc.l	AutoBluSx			; Indirizzo bob
+	dc.l	AutoBluSx_mask	; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	110				; x
+	dc.w	220				; y
+	dc.l	SingleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+
+	dc.l	FurgoneSx			; Indirizzo bob
+	dc.l	FurgoneSx_mask	; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	180				; x
+	dc.w	220				; y
+	dc.l	SingleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+	dc.l	AutoArancioneSx			; Indirizzo bob
+	dc.l	AutoArancioneSx_mask	; Indirizzo bobmask
+	dc.w	(48/16)			; Larghezza in word
+	dc.w	-1				; Velocità
+	dc.w	250				; x
+	dc.w	220				; y
+	dc.l	SingleFrameList	; Lista fotogrammi
+	dc.w	0				; Fotogramma attuale
+
+	dc.l	0
